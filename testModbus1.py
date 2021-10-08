f5642bfda46f87119a77b03a8f9d090ad8a5915d
yuukiwaw <zhangtuo1999@gmail.com>
2021-10-08 08:37:04 +0800

    add some tips

--- testModbus1.py
+++ testModbus1.py
@@ -6,16 +6,30 @@ from YukiAdamV import *
 # 封装的Adam类
 
 class AdamSet(object):
+    """adam6050 channels value change
+    Args:
+        object ([type]): [description]
+    """
     def __init__(self, ip ="192.168.0.198", port = "502", channel = "22"):
+        """init values
+        Args:
+            ip (str, optional): adammodbus ip setting. Defaults to "192.168.0.198".
+            port (str, optional): adammodbus port setting. Defaults to "502".
+            channel (str, optional): adammodbus channel do from 0-5 (channel 5 total is 22). Defaults to "22".
+        """
         self.ip = ip
         self.port = port
         self.channel = channel
 
     def setON(self):
+        """channel set value 1 (on)
+        """
         adam_modbus = AdamClass()
         adam_modbus.Adam6050Status(self.ip, self.port, self.channel, 1)
 
     def setOFF(self):
+        """channel set value 2 (off)
+        """
         adam_modbus = AdamClass()
         adam_modbus.Adam6050Status(self.ip, self.port, self.channel, 0)
         
